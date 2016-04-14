526c76e0b184fad82a3410ebbe84c7356d0cdb25
kimarre <kimarrexo@gmail.com>
2016-04-14 12:10:26 -0700

    Lab3: Finished

--- lab3.rkt
+++ lab3.rkt
@@ -63,28 +63,52 @@
 (define (desugar [s : ArithS]) : ArithC
   (match s
     [(numS n) (numC n)]
-    [(plusS l r) plusC (desugar l) (desugar r)]
+    [(plusS l r) (plusC (desugar l) (desugar r))]
     [(bminusS l r) (plusC (desugar l) (multC (numC -1) (desugar r)))] ; l - r = l + -1 * r
     [(uminusS n) (multC (numC -1) (desugar n))]
-    [(multS l r) (multC (desugar l) (desugar r))]
-    [other (error "Oh noes, bad input for ArithS")]))
+    [(multS l r) (multC (desugar l) (desugar r))]))
 
 (check-equal? (desugar (numS 5)) (numC 5))
+(check-equal? (desugar (plusS (numS 3) (numS 4))) (plusC (numC 3) (numC 4)))
+(check-equal? (desugar (bminusS (numS 6) (numS 3))) (plusC (numC 6) (multC (numC -1) (numC 3))))
+(check-equal? (desugar (uminusS (numS 4))) (multC (numC -1) (numC 4)))
+(check-equal? (desugar (multS (numS 4) (numS 4))) (multC (numC 4) (numC 4)))
+ 
+; Evaluates an Sexp by calling parse, desugar and interp
+(: top-interp [Sexp -> Number])
+(define (top-interp [s : Sexp])
+  (interp (desugar (parse s))))
 
+(check-equal? (top-interp '{+ 1 2}) 3)
+(check-equal? (top-interp '{- 6 2}) 4)
+(check-equal? (top-interp '{* 6 2}) 12)
 
-  ; CMD + \ makes λ
+; Applies the given function to the list twice
+(: doublemap ((Number -> Number) (Listof Number) -> (Listof Number)))
+(define (doublemap [f : (Number -> Number)] [l : (Listof Number)]) : (Listof Number)
+  (map (λ (number)
+      (f number))
+       (map (λ (number)
+              (f number))
+            l))
+  )
 
+; Example number manipulation function for doublemap
+(: triple-it (Number -> Number))
+(define (triple-it [num : Number]) : Number
+  (* 3 num))
 
+(check-equal? (doublemap triple-it (list 1 2 3)) (list 9 18 27))
 
+; Returns a list of lists where each element is a list of corresponding elements
+; from the original lists
+(: zip [(Listof Number) (Listof Number) -> (Listof (Listof Number))])
+(define (zip [l1 : (Listof Number)] [l2 : (Listof Number)])
+  (cond
+    [(empty? l1) empty]
+    [else (append (list (list (first l1) (first l2))) (zip (rest l1) (rest l2)))]))
 
-
-
-
-
-
-
-
-
+(check-equal? (zip (list 1 2 3) (list 4 5 6)) (list (list 1 4) (list 2 5) (list 3 6)))
 
 
 
