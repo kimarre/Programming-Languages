a3c8eaeb81456664521cd6ee7d97d37938abaeec
kimarre <kimarrexo@gmail.com>
2016-04-27 10:45:41 -0700

    A3: Finish converting to environments and closures
    Full test coverage for what I have now, no errors (yet)

--- Assignment3.rkt
+++ Assignment3.rkt
@@ -13,14 +13,13 @@
 (struct IdC (x) #:transparent)
 (struct AppC (fun args) #:transparent) ; use of a function (call)
 (struct ifleq0C (n then else) #:transparent)
-(struct FundefC (name args body) #:transparent)
 (struct BinopC (name left right) #:transparent)
 (struct LamC (param body) #:transparent) ; Symbol ExprC
 
 ;; Value that represents the result of evaluation:
 ; (define-type Value (U NumV CloV))
 (struct NumV (n) #:transparent) ; Real
-(struct CloV (param body env) #:transparent) ; Symbol ExprC Env
+(struct CloV (params body env) #:transparent) ; Symbol ExprC Env
 
 (define Operations (hash
                     'plus +
@@ -28,19 +27,19 @@
                     'minus -
                     'divide /))
 
-(define funs (list
-              (FundefC 'add-three (list 'y) (BinopC 'plus (IdC 'y) (NumC 3))) 
-              (FundefC 'double-plus-one (list 'x) (BinopC 'plus (BinopC 'mult (NumC 2) (IdC 'x)) (NumC 1)))
-              (FundefC 'add-two-nums (list 'x 'y) (BinopC 'plus (IdC 'x) (IdC 'y)))
-              (FundefC 'main (list) (NumC 7))
-              ))
+;(define funs (list
+;              (FundefC 'add-three (list 'y) (BinopC 'plus (IdC 'y) (NumC 3))) 
+;              (FundefC 'double-plus-one (list 'x) (BinopC 'plus (BinopC 'mult (NumC 2) (IdC 'x)) (NumC 1)))
+;              (FundefC 'add-two-nums (list 'x 'y) (BinopC 'plus (IdC 'x) (IdC 'y)))
+;              (FundefC 'main (list) (NumC 7))
+;              ))
 
-;(: interp-fns ((Listof FundefC) -> Real))
-(define (interp-fns complete-funs funs)
-  (cond
-    [(string=? (symbol->string (FundefC-name (first funs))) "main")
-     (interp (FundefC-body (first funs)) complete-funs)]
-    [else (interp-fns complete-funs (rest funs))]))
+;;(: interp-fns ((Listof FundefC) -> Real))
+;(define (interp-fns complete-funs funs)
+;  (cond
+;    [(string=? (symbol->string (FundefC-name (first funs))) "main")
+;     (interp (FundefC-body (first funs)) complete-funs)]
+;    [else (interp-fns complete-funs (rest funs))]))
 
 (define reserved-ops (list '\ '* '+ '- 'ifleq0))
 
@@ -63,6 +62,8 @@
 (define (parse sexp)
   (match sexp
     [(? real? n) (NumC n)]
+    [(list 'lam (list (? symbol? params) ...) body)
+     (LamC params (parse body))]
     [(? symbol? x)
      (cond
        [(not (isReserved? x))
@@ -76,9 +77,11 @@
      (cond
        [(not (isReserved? fun))
             (AppC (parse fun) (map parse arg))])]
-    [(list 'lam (list (? symbol? param)) body)
-     (LamC param (parse body))]
     [other (error "DFLY: Oh noes, input not well-formed")]))
+
+
+(check-equal? (parse '{lam {a b} 3}) (LamC (list 'a 'b) (NumC 3)))
+(check-equal? (parse '{lam {x y} {- x y}}) (LamC (list 'x 'y) (BinopC 'minus (IdC 'x) (IdC 'y))))
 
 (check-exn #px"DFLY: Oh noes, input not well-formed" (λ () (parse '(16))))
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(/ 3 4 5))))
@@ -99,83 +102,68 @@
 
 ;; parse an s-expression into a FundefC
 ;(: parse-fundef (Sexp -> FundefC))
-(define (parse-fundef s)
-  (match s
-    [(list 'func (? symbol? name) (list (? symbol? args) ...) body)
-     (cond
-       [(not (isReserved? name))
-        (FundefC name args (parse body))])]
-    [other
-     (error "DFLY: Oh noes, input to parse-fundef not well-formed")]))
-
-(check-equal? (parse-fundef '{func g {x} {* x 9}}) (FundefC 'g (list 'x) (BinopC 'mult (IdC 'x) (NumC 9))))
-(check-exn #px"DFLY: Oh noes, input to parse-fundef not well-formed" (λ () (parse-fundef '{func + () () 13})))
-(check-exn #px"DFLY: Cannot use reserved function name" (λ () (parse-fundef '{func + () 13})))
+;(define (parse-fundef s)
+;  (match s
+;    [(list 'func (? symbol? name) (list (? symbol? args) ...) body)
+;     (cond
+;       [(not (isReserved? name))
+;        (FundefC name args (parse body))])]
+;    [other
+;     (error "DFLY: Oh noes, input to parse-fundef not well-formed")]))
+;
+;(check-equal? (parse-fundef '{func g {x} {* x 9}}) (FundefC 'g (list 'x) (BinopC 'mult (IdC 'x) (NumC 9))))
+;(check-exn #px"DFLY: Oh noes, input to parse-fundef not well-formed" (λ () (parse-fundef '{func + () () 13})))
+;(check-exn #px"DFLY: Cannot use reserved function name" (λ () (parse-fundef '{func + () 13})))
 
 ;; Turns a list of s-expressions to a list of FundefC's
 ;(: parse-prog ((Listof Sexp) -> (Listof FundefC)))
-(define (parse-prog s)
-   (map parse-fundef s))
+;(define (parse-prog s)
+;   (map parse-fundef s))
+;
+;(define herpderpExp (FundefC 'herpderp (list 'a 'b) (BinopC 'plus (IdC 'a) (IdC 'b))))
+;(define meowExp (FundefC 'meow (list 'x 'y) (BinopC 'plus (BinopC 'mult (IdC 'x) (IdC 'y)) (NumC 1))))
+;
+;(check-equal? (parse-prog (list
+;                           '{func herpderp {a b} {+ a b}}
+;                           '{func meow {x y} {+ {* x y} 1}}))
+;              (list herpderpExp meowExp))
 
-(define herpderpExp (FundefC 'herpderp (list 'a 'b) (BinopC 'plus (IdC 'a) (IdC 'b))))
-(define meowExp (FundefC 'meow (list 'x 'y) (BinopC 'plus (BinopC 'mult (IdC 'x) (IdC 'y)) (NumC 1))))
 
-(check-equal? (parse-prog (list
-                           '{func herpderp {a b} {+ a b}}
-                           '{func meow {x y} {+ {* x y} 1}}))
-              (list herpderpExp meowExp))
-
-
-; ======================================= TODO: update binop with env
+; =======================================
 ;                Interp
 ; =======================================
+
+(define (interp-args args env)
+  (map (λ (x) (interp x env)) args))
 
 ;; Interpret an expression in a given environment
 ;(: interp (ExprC Env -> Value))
 (define (interp expr env)
   (match expr
     [(NumC n) (NumV n)]
-    [(LamC param body) (CloV param body env)]
-    [(IdC i) (hash-ref env i)]
+    [(LamC params body) (CloV params body env)]
+    [(IdC i) (hash-ref env i (λ () (error "DFLY: no value found for key" i)))]
     [(BinopC name l r)
      (define rVal (interp r env))
      (cond
-       [(and (= 0 (NumV-n rVal)) (equal? name 'divide)) (error "DFLY: Cannot divide by 0.")]
+       [(and (= 0 (NumV-n rVal)) (equal? name 'divide)) (error "DFLY: Cannot divide by 0")]
        [else (NumV ((hash-ref Operations name) (NumV-n (interp l env)) (NumV-n (interp r env))))])]
-
-    #;( ; This was our AppC for A2
-       [(AppC f a) (local ([define fd (get-fundef f funs)])
-                  (interp (foldl (λ (var val result)
-                                   (subst var val result))
-                                 (FundefC-body fd) ; initial value
-                                 (FundefC-args fd) ; vars to be sub'd
-                                 (map
-                                  (λ (x) (NumC (interp x funs))) ; vals to sub in
-                                  a))
-                          
-                          funs))]
-       )
-    
-    ; Instead of subst, use closure hash
-    ; This was very close, all the pieces are there, just confusing.
-    ; Separate into two helper functions for the map and foldl
-    ; Write good test cases to make sure each is doing what I think,
     [(AppC fun args)
      (match (interp fun env)
        [(CloV params body clo-env)
-        (interp (foldl (λ (var val result)
-                         (hash-set clo-env param
-                          (map (λ (x) ((interp x env)))
-                               args))
-                         body)])]
+        (interp body (foldl  ; returns a new env containing the param/arg pairs
+                      (λ (param arg new-env)
+                        (hash-set new-env param arg))
+                      clo-env
+                      params
+                      (interp-args args env)))])]
+    
+;    [(ifleq0C test then el)
+;     (cond
+;       [(<= (interp test env) 0) (interp then env)]
+;       [else (interp el env)])]
+    ))
 
-    [(ifleq0C test then el)
-     (cond
-       [(<= (interp test funs) 0) (interp then funs)]
-       [else (interp el funs)])]))
-
-
-(define (
 
 ;; Test cases for Interp
 (check-equal? (interp (parse '{+ 3 4}) empty-env) (NumV 7))
@@ -183,16 +171,33 @@
 
 (check-equal? (interp (parse '{v {+ 3 4}})
                       (hash
-                       'z (CloV 'q (parse '{+ q 1}) empty-env)
-                       'v (CloV 'q (parse '{+ q -1}) empty-env)))
+                       'z (CloV (list 'q) (parse '{+ q 1}) empty-env)
+                       'v (CloV (list 'q) (parse '{+ q -1}) empty-env)))
               (NumV 6))
 
-(check-exn #px"no value found for key"
+(check-equal? (interp (parse '{lam {a b} {+ a b}}) empty-env)
+              (CloV (list 'a 'b) (BinopC 'plus (IdC 'a) (IdC 'b)) empty-env))
+
+(check-exn #px"DFLY: Cannot divide by 0"
+           (λ ()
+             (interp (parse '{/ 1 0}) empty-env)))
+
+(check-exn #px"DFLY: no value found for key"
            (λ ()
              (interp (parse '{calls-b 3})
                      (hash
-                      'calls-b (CloV 'x (parse '{b 24}) empty-env)
-                      'b       (CloV 'y (parse '{+ x y}) empty-env)))))
+                      'calls-b (CloV (list 'x) (parse '{b 24}) empty-env)
+                      'b       (CloV (list 'y) (parse '{+ x y}) empty-env)))))
+
+
+;; Turn a DFLY3 value to a string
+; (: serialize [DFLY3 -> String])
+(define (serialize x)
+  (match x
+    [(NumC n) (~v n)]))
+
+(check-equal? (serialize (NumC 1)) "1")
+
 
 #;( ;old test cases for interp
    ;(check-exn #px"DFLY: Interp should not have encountered an IdC on its own. Undefined variable."
@@ -207,30 +212,31 @@
 
 ;; Evaluates an Sexp by calling parse and interp - Interpret main
 ;(: top-interp ((Listof Sexp) -> Real))
-(define (top-interp fun-sexps)
-  (interp-fns (parse-prog fun-sexps) (parse-prog fun-sexps)))
+;(define (top-interp fun-sexps)
+;  (interp-fns (parse-prog fun-sexps) (parse-prog fun-sexps)))
 
-(check-equal? (top-interp
-               '{{func add-two {x} {+ 2 x}}
-                 {func main {} {add-two 4}}}) 6)
+;(check-equal? (top-interp
+;               '{{func add-two {x} {+ 2 x}}
+;                 {func main {} {add-two 4}}}) 6)
+;
+;(check-equal? (top-interp
+;               '{{func add-together {x y} {+ y x}}
+;                 {func main {} {add-together 4 2}}}) 6)
+;
+;(check-equal? (top-interp
+;               '{{func isLessThan1 {x} {ifleq0 x 1 0}}
+;                 {func main {} {isLessThan1 3}}}) 0)
+;
+;(check-exn #px"DFLY: Cannot divide by 0" (λ () (top-interp
+;                                                '{{func divideByZero {x} (/ x 0)}
+;                                                  (func main () (divideByZero 2))})))
+;
+;(check-exn #px"DFLY: Cannot divide by 0" (λ () (top-interp
+;                                                '{{func ignoreit (x) (+ 3 4)}
+;                                                  (func main () (ignoreit (/ 1 0)))})))
 
-(check-equal? (top-interp
-               '{{func add-together {x y} {+ y x}}
-                 {func main {} {add-together 4 2}}}) 6)
 
-(check-equal? (top-interp
-               '{{func isLessThan1 {x} {ifleq0 x 1 0}}
-                 {func main {} {isLessThan1 3}}}) 0)
-
-(check-exn #px"DFLY: Cannot divide by 0" (λ () (top-interp
-                                                '{{func divideByZero {x} (/ x 0)}
-                                                  (func main () (divideByZero 2))})))
-
-(check-exn #px"DFLY: Cannot divide by 0" (λ () (top-interp
-                                                '{{func ignoreit (x) (+ 3 4)}
-                                                  (func main () (ignoreit (/ 1 0)))})))
-
-
+; binop as function is valid for parser but not interpreter
 ; nested functions are just lamc's in the body of another lamc
 ; {lam {x} {lam {y} {+ x y}}}
 ; --->
