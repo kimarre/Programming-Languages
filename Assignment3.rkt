58ead564470aee3b8abb9dad6b21c7857ca722da
kimarre <kimarrexo@gmail.com>
2016-04-27 21:57:01 -0700

    A3: Add serialize function & some top-interp tests

--- Assignment3.rkt
+++ Assignment3.rkt
@@ -3,6 +3,7 @@
 
 ; Author: Kim Arre (karre@calpoly.edu)
 ; CPE 430 - Spring 2016
+; =====================================
 
 ;; an Environment is a hash table mapping symbols to values
 ;(define-type Env (HashTable Symbol Value))
@@ -12,16 +13,19 @@
 (struct NumC (n) #:transparent)
 (struct IdC (x) #:transparent)
 (struct AppC (fun args) #:transparent) ; use of a function (call)
-(struct ifC (n then else) #:transparent)
+(struct ifC (test then else) #:transparent)
 (struct BinopC (name left right) #:transparent)
 (struct LamC (param body) #:transparent) ; Symbol ExprC
 (struct BooleanC (b) #:transparent)
 
+
 ;; Value that represents the result of evaluation:
-; (define-type Value (U NumV CloV))
+; (define-type Value (U NumV CloV BooleanV))
 (struct NumV (n) #:transparent) ; Real
 (struct CloV (params body env) #:transparent) ; Symbol ExprC Env
+(struct BooleanV (val) #:transparent)
 
+;; Operations and uses for interp to look up
 (define Operations (hash
                     'plus +
                     'mult *
@@ -30,7 +34,7 @@
                     'eq eq?
                     '<= <=))
 
-(define reserved-ops (list '\ '* '+ '- 'ifleq0))
+(define reserved-ops (list '\ '* '+ '- 'if 'eq '<= 'with 'true 'false 'lam))
 
 ; Determine if a symbol being used is part of our language already
 (define (isReserved? x)
@@ -87,15 +91,15 @@
 (check-equal? (parse '{lam {a b} true}) (LamC (list 'a 'b) (BooleanC 'true)))
 (check-equal? (parse '{lam {a b} 3}) (LamC (list 'a 'b) (NumC 3)))
 (check-equal? (parse '{lam {x y} {- x y}}) (LamC (list 'x 'y) (BinopC 'minus (IdC 'x) (IdC 'y))))
+(check-equal? (parse '{if {eq? 2 2} 1 0}) (ifC (BinopC 'eq (NumC 2) (NumC 2)) (NumC 1) (NumC 0)))
 
 (check-exn #px"DFLY: Oh noes, input not well-formed" (λ () (parse '(16))))
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(/ 3 4 5))))
-
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(+ cat dog poop))))
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(* 3))))
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(- 3 4 5))))
-;(check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(ifleq0 3 cat))))
 (check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(+ / 3))))
+(check-exn #px"DFLY: Cannot use reserved function name." (λ () (parse '(if / 3))))
 
 (check-equal? (parse '{+ 1 2}) (BinopC 'plus (NumC 1) (NumC 2)))
 (check-equal? (parse '{* 2 3}) (BinopC 'mult (NumC 2) (NumC 3)))
@@ -127,6 +131,7 @@
      (cond
        [(and (= 0 (NumV-n rVal)) (equal? name 'divide)) (error "DFLY: Cannot divide by 0")]
        [else (NumV ((hash-ref Operations name) (NumV-n (interp l env)) (NumV-n (interp r env))))])]
+    [(BooleanC bool) (BooleanV bool)]
     [(AppC fun args)
      (match (interp fun env)
        [(CloV params body clo-env)
@@ -139,6 +144,7 @@
 
 
 ;; Test cases for Interp
+;(check-equal? (interp (parse 'true)) '
 (check-equal? (interp (parse '{+ 3 4}) empty-env) (NumV 7))
 (check-equal? (interp (parse '{* 3 {+ 4 5}}) empty-env) (NumV 27))
 
@@ -152,24 +158,46 @@
               (CloV (list 'a 'b) (BinopC 'plus (IdC 'a) (IdC 'b)) empty-env))
 
 (check-exn #px"DFLY: Cannot divide by 0"
-           (λ ()
-             (interp (parse '{/ 1 0}) empty-env)))
+           (λ () (interp (parse '{/ 1 0}) empty-env)))
 
 (check-exn #px"DFLY: no value found for key"
-           (λ ()
-             (interp (parse '{calls-b 3})
-                     (hash
-                      'calls-b (CloV (list 'x) (parse '{b 24}) empty-env)
-                      'b       (CloV (list 'y) (parse '{+ x y}) empty-env)))))
-
+           (λ () (interp (parse '{calls-b 3})
+                         (hash
+                          'calls-b (CloV (list 'x) (parse '{b 24}) empty-env)
+                          'b       (CloV (list 'y) (parse '{+ x y}) empty-env)))))
 
 ;; Turn a DFLY3 value to a string
-; (: serialize [DFLY3 -> String])
+; (: serialize (DFLY3 -> String))
 (define (serialize x)
   (match x
-    [(NumC n) (~v n)]))
+    [(NumV n) (~v n)]
+    [(CloV params body env) "<#procedure>"]
+    [(BooleanV bool)
+     (cond
+       [(equal? bool 'true) "true"]
+       [(equal? bool 'false) "false"])]))
 
-(check-equal? (serialize (NumC 1)) "1")
+(check-equal? (serialize (NumV 1)) "1")
+(check-equal? (serialize (CloV (list 'x) (parse '{+ 1 2}) empty-env)) "<#procedure>")
+(check-equal? (serialize (BooleanV 'true)) "true")
+(check-equal? (serialize (BooleanV 'false)) "false")
+
+;; Combine parsing and evaluation
+; (: top-eval (sexp -> string)
+(define (top-eval s)
+  (serialize (interp (parse s) empty-env)))
+
+(check-equal? (top-eval '{+ 2 3}) "5")
+(check-equal? (top-eval 'true) "true")
+
+
+;(define env1
+;  'meow (CloV (list 'q) (parse '{+ q 1}) empty-env))
+
+;(define env2
+;  'moo (NumV 3)
+;  'woof (CloV (list 'q) (parse '{+ q -1}) empty-env))
+
 
 
 #;( ;old test cases for interp
